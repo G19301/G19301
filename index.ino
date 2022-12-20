7923f6407b01ebba968b2b8637f8a93aa5a21e21
G19301 <120381206+G19301@users.noreply.github.com>
2022-12-20 23:14:43 +0200

    Add files via upload

---
+++ index.ino
@@ -0,0 +1,164 @@
+#include <WiFi.h>
+#include "ThingSpeak.h"  // always include thingspeak header file after other header files and custom macros
+
+#define SECRET_SSID "youssef"   // replace MySSID with your WiFi network name
+#define SECRET_PASS "12345678"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1883496                    // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "1G6P98QK2TG8HLJU"  // replace XYZ with your channel write API Key
+
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password
+int keyIndex = 0;           // your network key Index number (needed only for WEP)
+WiFiClient client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char* myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+String myStatus = "";
+
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float Celcius = 0;
+float Fahrenheit = 0;
+
+
+#define TdsSensorPin 36
+#define VREF 3.3   // analog reference voltage(Volt) of the ADC
+#define SCOUNT 30  // sum of sample point
+
+int analogBuffer[SCOUNT];  // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
+
+float averageVoltage = 0;
+float tdsValue = 0;
+float temperature = 25;  // current temperature for compensation
+static unsigned long analogSampleTimepoint;
+static unsigned long printTimepoint;
+
+// median filtering algorithm
+int getMedianNum(int bArray[], int iFilterLen) {
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0) {
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  } else {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TdsSensorPin, INPUT);
+  analogSampleTimepoint = millis();
+  printTimepoint = millis();
+  sensors.begin();
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+  tdsReading();
+  tempReading();
+ sendData(tdsValue, Celcius);
+  delay(30000);
+}
+
+void tdsReading() {
+  if (millis() - analogSampleTimepoint > 40U) {  //every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);  //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if (analogBufferIndex == SCOUNT) {
+      analogBufferIndex = 0;
+    }
+  }
+
+  if (millis() - printTimepoint > 800U) {
+    printTimepoint = millis();
+    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++) {
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+
+      // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0;
+
+      //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+      float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+      //temperature compensation
+      float compensationVoltage = averageVoltage / compensationCoefficient;
+
+      //convert voltage value to tds value
+      tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+
+      //Serial.print("voltage:");
+      //Serial.print(averageVoltage,2);
+      //Serial.print("V   ");
+      Serial.print("TDS Value:");
+      Serial.print(tdsValue, 0);
+      Serial.println("ppm");
+    }
+  }
+  delay(30000);
+}
+
+void tempReading() {
+  sensors.requestTemperatures();
+  Celcius = sensors.getTempCByIndex(0);
+  Fahrenheit = sensors.toFahrenheit(Celcius);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  Serial.print(" F  ");
+  Serial.println(Fahrenheit);
+  delay(30000);
+}
+
+void sendData(float tds, float temp) {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(30000);
+    }
+    Serial.println("\nConnected.");
+  }
+
+  // set the fields with the values
+  ThingSpeak.setField(1, tds);
+  ThingSpeak.setField(2, temp);
+
+  ThingSpeak.setStatus(myStatus);
+
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if (x == 200) {
+    Serial.println("Channel update successful.");
+  } else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+}
